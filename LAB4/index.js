b8dad9b7ee935c0fe7b7abe2dff9dcd36fc5eb4b
2017129007 <crazydog4505@yonsei.ac.kr>
2023-05-13 16:24:05 +0900

    feat : fetch test consolelog

--- LAB4/index.js
+++ LAB4/index.js
@@ -1,3 +1,28 @@
-const booksResponse = fetch("./products.json");
+fetch("./products.json")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
 
-console.log(booksResponse);
+function createBooksDiv(booksResponse) {
+  const booksRow = document.createElement(div);
+  const books = document.createElement(div);
+  const bookImg = document.createElement(img);
+  const bookNameWrapper = document.createElement(div);
+  const bookName = document.createElement(img);
+}
+
+// <div class="books-row d-flex-row">
+{
+  /* <div class="books">
+<img class="book-image" src="./img/effectiveTypescript.jpg" />
+<div class="book-name-wrapper">
+  <!-- <span class="book-name">어떤책1</span> -->
+</div>
+</div>
+<div class="books">
+<img class="book-image" src="./img/effectiveTypescript.jpg" />
+<div class="book-name-wrapper">
+  <!-- <span class="book-name">어떤책2</span> -->
+</div>
+</div>
+</div> */
+}
